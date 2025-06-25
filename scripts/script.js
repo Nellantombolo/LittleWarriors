f029b9b2b3d6123daa710a4a248c04b522a9d8ac
Rachelkab <149601339+Rachelkab@users.noreply.github.com>
2025-06-25 14:13:04 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
+/*
+Student Name: Rachel Kabwiza
+File Name: script.js
+Date: 06/25/2025
+*/
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+		} else {
+		    menu.style.display = "block";
+		}
+}
 
