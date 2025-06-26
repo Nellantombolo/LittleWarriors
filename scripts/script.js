e3ef441e0a3c4129b8123bba041a470e09165a9a
Sena <corakcisena@gmail.com>
2025-06-26 17:17:45 -0600

    script.js

--- scripts/script.js
+++ scripts/script.js
@@ -21,3 +21,17 @@ document.addEventListener("DOMContentLoaded", function () {
 	});
 });
 
+/*
+Student Name: Sena Corakci
+File Name: script.js
+Date: 06/26/2025
+*/ 
+// Toggle mobile menu - added by Sena
+function toggleMenu() {
+  const menu = document.getElementById("menu-links");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
